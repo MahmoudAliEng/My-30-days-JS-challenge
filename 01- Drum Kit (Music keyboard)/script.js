330438821d3e762abba9451c66b9c8de1353c2c2
Sid Ali MAHMOUDI <eng.sidali.mahmoudi@gmail.com>
2020-06-02 23:23:32 +0100

    Removing transform after the end of the transition

--- 01- Drum Kit (Music keyboard)/script.js
+++ 01- Drum Kit (Music keyboard)/script.js
@@ -12,5 +12,19 @@ function playSound (event) {
     key.classList.add('playing');
   }
 
+// Remove transition after its end
+function removeTransition (event) {
+    if(event.propertyName !== 'transform') return; // skip it if it's not a transform
+    
+    // console.log(this); will return the current key div (the clicked key div)
+    this.classList.remove('playing'); // Remove the transition effect at the its end from the clicked key
+}
+
 // Listtening when keys pressed to join the correct audio file
 window.addEventListener('keydown', playSound);
+
+// Remove the transition after clicking a key
+// Select all keys
+const keys = document.querySelectorAll('.key');
+// Add event listener for each key to remove the transition effects made by the CSS after the end of the transition
+keys.forEach( key => key.addEventListener('transitionend', removeTransition));
