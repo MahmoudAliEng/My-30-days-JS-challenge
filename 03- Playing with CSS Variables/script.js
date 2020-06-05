60e39c7ae9f5ec3ccf6cdd855f8956c9f49202cd
Sid Ali MAHMOUDI <eng.sidali.mahmoudi@gmail.com>
2020-06-05 01:00:56 +0100

    Setting CSS variables to the new values from JS

--- 03- Playing with CSS Variables/script.js
+++ 03- Playing with CSS Variables/script.js
@@ -3,7 +3,16 @@ const inputs = document.querySelectorAll('.controls input');
 console.log(inputs);
 
 function handleUpdates(){
-    console.log(this.value);
+    const suffix = this.dataset.sizing || ''; // dataset attribute contains the data HTML attribute content
+    let styleSheet = document.styleSheets[0]; // Getting the stylesheet document
+    styleSheet.cssRules[0].style.cssText += `--${this.name} : ${this.value}${suffix};` // Setting the inputs equal to the new value entered
+    // The name and the value are the input name's and value's respect.
+    /* 
+    If the CSS was made inside HTML (style tag) then the code were to be like this:
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    */    
+    
+    
 }
 
 inputs.forEach( input => input.addEventListener('change', handleUpdates));
