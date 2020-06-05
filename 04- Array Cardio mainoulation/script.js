dbf4abd0237c8f84e5b4729fd20a6896ca0bbadc
Sid Ali MAHMOUDI <eng.sidali.mahmoudi@gmail.com>
2020-06-05 23:58:41 +0100

    Answering the second question

--- 04- Array Cardio mainoulation/script.js
+++ 04- Array Cardio mainoulation/script.js
@@ -25,10 +25,11 @@
       const firstQuest = inventors.filter(inventor => (inventor.year >= 1500 && inventor.year < 1600));
       console.table(firstQuest);
 
-      
+
       // Array.prototype.map()
       // 2. Give us an array of the inventors first and last names
-  
+      const secondQuest = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+      console.table(secondQuest);
       // Array.prototype.sort()
       // 3. Sort the inventors by birthdate, oldest to youngest
   
