5d7dc0b44e501075bf7e6ef2cd64d5184a8ca444
Sid Ali MAHMOUDI <eng.sidali.mahmoudi@gmail.com>
2020-06-09 23:56:16 +0100

    Linking needed HTML elements

--- 06- Ajax type ahead/script.js
+++ 06- Ajax type ahead/script.js
@@ -1,13 +1,17 @@
 // A url to a JSON data (an array) of 1000 US cities
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
+// Linking the  search bar and the suggestions list
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
 // The coming data from the web
 const cities = [];
 
 // Fetching JSON data from URL and put it in a new array
 fetch(endpoint)
    .then(blob => blob.json())
-   .then(data => cities.push(...data));
+   .then(data => cities.push(...data)); // To get elements inserted directly not array inserted as one element
 
 // Function that return the matches cities or states case-insensative for a research word
 function findMatches(wordMatch, cities) {
@@ -16,3 +20,4 @@ function findMatches(wordMatch, cities) {
         return place.state.match(regex) || place.city.match(regex);
     });
 }
+
