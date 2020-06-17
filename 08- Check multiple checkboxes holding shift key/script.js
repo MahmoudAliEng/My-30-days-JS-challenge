92ff3e154cd2f1fc4177868c8556c66eaa357801
Sid Ali MAHMOUDI <eng.sidali.mahmoudi@gmail.com>
2020-06-17 22:55:20 +0100

    Detecting starting and ending of checkboxes

--- 08- Check multiple checkboxes holding shift key/script.js
+++ 08- Check multiple checkboxes holding shift key/script.js
@@ -7,7 +7,10 @@ let lastChecked;
 function handleClick(event){
     // Check if the user is clicking on shift key and the input element is checked
     if(event.shiftKey && event.target.checked){
-        
+        let inBetween = false;
+        checkBoxes.forEach(checkBox => {
+            if( checkBox === this || checkBox === lastChecked) inBetween != inBetween;
+        });
     }
     console.log(event);
     lastChecked = this;
