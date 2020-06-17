6fee1910bf8ad46fac564f6e212dc23507de5f1a
Sid Ali MAHMOUDI <eng.sidali.mahmoudi@gmail.com>
2020-06-17 22:24:32 +0100

    Checking if the box is checked and shift key is hitten

--- 08- Check multiple checkboxes holding shift key/script.js
+++ 08- Check multiple checkboxes holding shift key/script.js
@@ -1,9 +1,16 @@
 const checkBoxes = document.querySelectorAll('.inbox input[type=checkbox]');
 
-console.log(checkBoxes);
+
+let lastChecked;
+
 
 function handleClick(event){
+    // Check if the user is clicking on shift key and the input element is checked
+    if(event.shiftKey && event.target.checked){
+        
+    }
     console.log(event);
+    lastChecked = this;
 }
 
 checkBoxes.forEach(checkBox => checkBox.addEventListener('click', handleClick));
