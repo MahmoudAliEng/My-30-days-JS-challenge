ad120368779d7c1f0b8fcc80bbf575f24ef85850
Sid Ali MAHMOUDI <eng.sidali.mahmoudi@gmail.com>
2020-06-19 22:50:36 +0100

    Adding fixed or changing color functionality

--- 07- HTML canvas/script.js
+++ 07- HTML canvas/script.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector("#draw");
+const changinColor = document.getElementById("changing-color-checkbox");
 
 // Getting the context which is kind of the frame we will work on it
 const ctx = canvas.getContext("2d");
@@ -6,7 +7,9 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-ctx.strokeStyle = "#BADA55";
+let color = "#BADA55"
+
+
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
 ctx.lineWidth = 50;
@@ -18,8 +21,19 @@ let hue = 0; // To apply the idea of changing the color while drawing
 let increasingWidth = true;
 
 
-
 function draw(e){
+    if(!isDrawing) return; // stop running if the user is not clicking the mouse down
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+
+    [lastX, lastY] = [e.offsetX, e.offsetY]; // Using destructuring array to update our lastX and Y
+}
+
+
+function drawChangingColor(e){
     if(!isDrawing) return; // stop running if the user is not clicking the mouse down
     ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     ctx.beginPath();
@@ -29,12 +43,12 @@ function draw(e){
     hue++;
     if (hue >= 360) hue = 0;
 
-    if (ctx.lineWidth >= 80 || ctx.lineWidth <= 1) {increasingWidth = !increasingWidth};
+    /* if (ctx.lineWidth >= 80 || ctx.lineWidth <= 1) {increasingWidth = !increasingWidth};
     if(increasingWidth) {
         ctx.lineWidth++;
     } else {
         ctx.lineWidth--;
-    }
+    } */
 
     [lastX, lastY] = [e.offsetX, e.offsetY]; // Using destructuring array to update our lastX and Y
      
@@ -54,9 +68,17 @@ canvas.addEventListener('mousedown', (e) => {
     [lastX, lastY] = [e.offsetX, e.offsetY]; // To start drawing where the cursor is now not from 0
 }); 
 
-canvas.addEventListener("contextmenu", erase);
+canvas.addEventListener("contextmenu", erase); // erase everything whenclicking the right button of the mouse
 
-canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousemove', (e) => {
+    if (changinColor.checked){
+        drawChangingColor(e);
+        // return '';
+    }
+    else {
+        draw(e);
+    }
+});
 canvas.addEventListener('mouseup', () => isDrawing = false); // And trigger off when they released the mouse up
 canvas.addEventListener('mouseout', () => isDrawing = false); // Trigger off also when they click the mouse but out of the browser
 
